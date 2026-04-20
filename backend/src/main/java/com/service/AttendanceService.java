d83361daa7caf4bb5d466ba516482a1f074f6ba2
ManasiLangore <manasilangore1125@gmail.com>
2026-04-21 00:41:37 +0530

    attendace feature added

---
+++ backend/src/main/java/com/service/AttendanceService.java
@@ -0,0 +1,41 @@
+package com.service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.entity.Attendance;
+import com.repository.AttendanceRepository;
+
+public class AttendanceService {
+    
+    @Autowired
+    AttendanceRepository repo;
+
+
+    // Employee action
+    public Attendance punchIn(int empId){
+        Attendance att = new Attendance();
+
+        att.setEmpId(empId);
+        att.setDate(LocalDate.now());
+        att.setPunchInTime(LocalDateTime.now());
+        att.setStatus("PENDING");
+
+        return repo.save(att);
+    }
+
+    //Admin action- view pending
+    public List<Attendance> getPending(){
+        return repo.findByStatus("PENDING");
+    }
+
+    //Admin action - approve/reject
+    public Attendance updateStatus(int id, String status){
+        Attendance att = repo.findById(id).orElseThrow();
+        att.setStatus(status);
+        return repo.save(att);
+    }
+}
