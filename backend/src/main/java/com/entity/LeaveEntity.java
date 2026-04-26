745f2dfb064b08f52779272204f54fef401283a6
ManasiLangore <manasilangore1125@gmail.com>
2026-04-26 23:18:00 +0530

    Started with Leave feature

---
+++ backend/src/main/java/com/entity/LeaveEntity.java
@@ -0,0 +1,107 @@
+package com.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class LeaveEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "empid", nullable = false)
+    private EmployeeEntity empid;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String leaveType;
+    private String reason;
+
+    @Enumerated(EnumType.STRING)
+    private LeaveStatus status = LeaveStatus.PENDING;
+
+    public enum LeaveStatus{
+        PENDING,
+        APPROVED,
+        REJECTED
+    }
+
+    private String adminRemark;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public EmployeeEntity getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(EmployeeEntity empid) {
+        this.empid = empid;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getLeaveType() {
+        return leaveType;
+    }
+
+    public void setLeaveType(String leaveType) {
+        this.leaveType = leaveType;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public LeaveStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(LeaveStatus status) {
+        this.status = status;
+    }
+
+    public String getAdminRemark() {
+        return adminRemark;
+    }
+
+    public void setAdminRemark(String adminRemark) {
+        this.adminRemark = adminRemark;
+    }
+
+    
+    
+}
