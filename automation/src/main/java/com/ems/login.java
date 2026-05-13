92f1181d98c4b16588f467ed9ab09a7dbac1561d
ManasiLangore <manasilangore1125@gmail.com>
2026-05-13 08:42:27 +0530

    Initial commit - automation framework setup

---
+++ automation/src/main/java/com/ems/login.java
@@ -0,0 +1,15 @@
+package com.ems;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class login {
+    public static void main(String[] args) {
+//        System.setProperty("webdriver.chrome.driver", "D:\\HefShine\\Automation Testing\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+
+        //initialize
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://localhost:3000/");
+    }
+}
