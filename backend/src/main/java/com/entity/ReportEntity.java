241b283a3c5332a3cb105b5a61f95e2f7a6ae8ff
ManasiLangore <manasilangore1125@gmail.com>
2026-05-16 20:49:38 +0530

    started with new feature report

---
+++ backend/src/main/java/com/entity/ReportEntity.java
@@ -0,0 +1,103 @@
+package com.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "reports")
+public class ReportEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+    private String description;
+    private String category;
+    private String status;
+    private String adminRemark;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    //Many reports can be filed by one employee
+    @ManyToOne
+    @JoinColumn(name = "empid")
+    private EmployeeEntity emploeyee;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAdminRemark() {
+        return adminRemark;
+    }
+
+    public void setAdminRemark(String adminRemark) {
+        this.adminRemark = adminRemark;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public EmployeeEntity getEmploeyee() {
+        return emploeyee;
+    }
+
+    public void setEmploeyee(EmployeeEntity emploeyee) {
+        this.emploeyee = emploeyee;
+    }
+
+    
+
+}
