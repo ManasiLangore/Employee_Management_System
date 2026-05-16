0fb4c6ea23d4751c495e58cf9d2fa0d51243d21c
ManasiLangore <manasilangore1125@gmail.com>
2026-05-16 20:50:38 +0530

    login check

--- automation/src/main/java/com/ems/login.java
+++ automation/src/main/java/com/ems/login.java
@@ -1,15 +1,24 @@
 package com.ems;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class login {
     public static void main(String[] args) {
-//        System.setProperty("webdriver.chrome.driver", "D:\\HefShine\\Automation Testing\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-
         //initialize
         WebDriver driver = new ChromeDriver();
 
-        driver.get("http://localhost:3000/");
+        driver.get("http://localhost:3000/login");
+
+        Actions actions = new Actions(driver);
+
+        WebElement clkU = driver.findElement(By.name("username"));
+        actions.click(clkU).perform();
+
+        WebElement clkP = driver.findElement(By.name("password"));
+        actions.click(clkP).perform();
     }
 }
